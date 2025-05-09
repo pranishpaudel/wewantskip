d82013cf73d9d2aa8f267f42c2ba644f27448ea1
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2025-05-09 11:17:39 +0545

    feat: initialize Next.js project with TypeScript and Tailwind CSS
    - Add package.json with dependencies for React, Next.js, TypeScript, and Tailwind CSS.
    - Create postcss.config.mjs for Tailwind CSS integration.
    - Add SVG assets for icons and logos in the public directory.
    - Set up tsconfig.json for TypeScript configuration with Next.js support.

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "We Want Skip",
+  description: "A website for skip",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black h-screen w-screen`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
