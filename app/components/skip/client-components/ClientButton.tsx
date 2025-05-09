f61304a37a8e355202cf2b439d52beba37c70469
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2025-05-09 17:45:21 +0545

    fix: update ClientButton layout for improved styling and alignment

--- app/components/skip/client-components/ClientButton.tsx
+++ app/components/skip/client-components/ClientButton.tsx
@@ -10,12 +10,14 @@ type ClientButtonProps = {
 
 const ClientButton: React.FC<ClientButtonProps> = ({ onClick, skipSize }) => {
   return (
-    <Button
-      onClick={() => onClick()}
-      className="w-full py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-    >
-      Select {skipSize} Yard Skip
-    </Button>
+    <div className="flex justify-center items-center w-full">
+      <Button
+        onClick={() => onClick()}
+        className="w-3/5 py-2 bg-white text-white font-semibold rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+      >
+        Select {skipSize} Yard Skip
+      </Button>
+    </div>
   );
 };
 
