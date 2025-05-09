23a47d69af4dcfb50ca5b20e70292b33ab58b175
Pranish Poudel <122670872+pranishpaudel@users.noreply.github.com>
2025-05-09 18:33:27 +0545

    fix: update SectionHeading and SkipCard components for improved styling and consistency

--- app/components/skip/SectionHeading.tsx
+++ app/components/skip/SectionHeading.tsx
@@ -8,10 +8,10 @@ interface SectionHeadingProps {
 const SectionHeading: React.FC<SectionHeadingProps> = ({ title, subtitle }) => {
   return (
     <div className="text-center py-4 md:py-6 lg:py-8 bg-black">
-      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white mb-3 md:mb-4 font-poppins">
+      <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-neutral-300 mb-3 md:mb-4 font-poppins">
         {title}
       </h1>
-      <p className="text-base sm:text-lg md:text-xl text-gray-300 font-opensans max-w-xl mx-auto px-4">
+      <p className="text-sm sm:text-base md:text-lg text-gray-300 font-opensans max-w-xl mx-auto px-4">
         {subtitle}
       </p>
     </div>
